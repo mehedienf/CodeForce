823f390123b5db78f0e1486c85bcd4aa85725f2b
mehedienf <enammehedi06@gmail.com>
2025-08-23 19:48:59 +0600

    upload

--- Contests/1043 Div. 3/temp.cpp
+++ Contests/1043 Div. 3/temp.cpp
@@ -1,30 +1,34 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
+
+typedef long long ll;
 
 int main() {
     int t;
     cin >> t;
-    while (t--) {
-        long long n;
+    while (t) {
+        ll n;
         cin >> n;
-        vector<long long> res;
-        for (int k = 1; ; ++k) {
-            long long d = 1;
-            for (int i = 0; i < k; ++i) d *= 10;
-            long long denom = 1 + d;
-            if (denom > n) break; // No need to check further
-            if (n % denom == 0) {
-                res.push_back(n / denom);
+        vector<ll> powers;
+        ll x = 0, temp = n;
+        // Find powers of 3 needed (base-3 representation)
+        while (temp > 0) {
+            powers.push_back(temp % 3);
+            temp /= 3;
+        }
+        ll cost = 0;
+        for (ll i = 0; i < powers.size(); ++i) {
+            for (ll j = 0; j < powers[i]; ++j) {
+                // If digit is 1 or 2, we need 1 or 2 deals of size 3^i
+                ll deal_size = 1;
+                for (ll k = 0; k < i; ++k) deal_size *= 3;
+                // Cost formula: 3^(i+1) + i * 3^(i-1)
+                ll deal_cost = deal_size * 3 + (i == 0 ? 0 : i * (deal_size / 3));
+                cost += deal_cost;
             }
         }
-        if (res.empty()) {
-            cout << "0\n";
-        } else {
-            cout << res.size() << "\n";
-            for (auto x : res) cout << x << " ";
-            cout << "\n";
-        }
+        cout << cost << endl;
+        t--;
     }
     return 0;
 }
