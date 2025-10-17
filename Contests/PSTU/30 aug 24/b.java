1b5c698ef35ba36a4aec614c32ce70119b87e6c5
mehedienf <enammehedi06@gmail.com>
2025-10-17 20:35:12 +0600

    Remove unnecessary files from PSTU contest directory

--- Contests/PSTU/30 aug 24/b.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class b {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n, sum = 0;
-        int[] arr
-
-    }
-}
